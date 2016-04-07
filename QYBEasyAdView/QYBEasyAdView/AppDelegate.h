e393ac1ef3223980ce92a3ce9bbca82c3ca22ffd
qianyb <qianyb@woshiqyb.local>
2016-04-07 13:29:43 +0800

    初始化工程

---
+++ QYBEasyAdView/QYBEasyAdView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYBEasyAdView
+//
+//  Created by qianyb on 16/4/7.
+//  Copyright © 2016年 qianyb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QYBEasyAdView/QYBEasyAdView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYBEasyAdView
+//
+//  Created by qianyb on 16/4/7.
+//  Copyright © 2016年 qianyb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
